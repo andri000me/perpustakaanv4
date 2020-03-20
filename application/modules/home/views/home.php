b540b33ceda9a737f5dec28a3405a8a37f6a7735
rekysda <rekysda@gmail.com>
2020-03-20 08:54:50 +0700

    20200320

--- application/modules/home/views/home.php
+++ application/modules/home/views/home.php
@@ -1,9 +1,25 @@
+<style>
+a:link, a:visited {
+	width:175px;
+    color: white;
+    padding: 14px 25px;
+    text-align: center; 
+    text-decoration: none;
+    display: inline-block;
+	border:1px solid white;
+}
+
+a:hover, a:active {
+    background-color: white;
+	color:black;
+}
+</style>
 <main role="main" class="inner cover mt-auto">
 	<h1 class="cover-heading"><?= $title; ?></h1>
 	<p class="lead">Sistem Informasi Perpustakaan</p>
-	<p class="lead">
-		<a href="<?= base_url('opac') ?>" class="btn btn-lg btn-secondary">Opac</a>
-		<a href="<?= base_url('pengunjung') ?>" class="btn btn-lg btn-secondary">Pengunjung</a>
-		<a href="<?= base_url('loginperpustakaan') ?>" class="btn btn-lg btn-secondary">Login Perpustakaan</a>
-	</p>
+
+		<a href="<?= base_url('opac') ?>">Opac</a>
+		<a href="<?= base_url('pengunjung') ?>">Pengunjung</a>
+		<a href="<?= base_url('loginperpustakaan') ?>">User</a>
+
 </main>
